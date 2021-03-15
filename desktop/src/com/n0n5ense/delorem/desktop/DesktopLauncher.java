968cfa2949809608423b6a8be4fc266d2939a515
NonSense <kawaisa.taritenai@gmail.com>
2021-03-16 04:23:00 +0900

    Inital

---
+++ desktop/src/com/n0n5ense/delorem/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.n0n5ense.delorem.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.n0n5ense.delorem.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
